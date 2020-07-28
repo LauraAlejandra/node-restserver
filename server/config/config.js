9b996dc5212487eb713321f24102f7a1a33378f9
Laura <lauraalee1998@gmail.com>
2020-07-27 22:25:44 -0500

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -8,10 +8,15 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; //si la variable process.e
 
 //BASE DE DATOS
 let urlBD;
-//if (process.env.NODE_ENV === 'dev') {
-//  urlBD = 'mongodb://localhost:27017/cafe';
-//} else {
-urlBD = 'mongodb+srv://admin:Zarzamoramora@cluster0.cgsgb.mongodb.net/cafe?retryWrites=true&w=majority';
-//}
+if (process.env.NODE_ENV === 'dev') {
+    urlBD = 'mongodb://localhost:27017/cafe';
+} else {
+    /*
+    Para que las credenciales de la liga no esten a la vista, podemos crear una variable de entorno en heroku con el comando
+    heroku config:set MONGO_URI="mongodb+srv://admin:<password>@cluster0.cgsgb.mongodb.net/<nombreBD>?retryWrites=true&w=majority"
+    y poner process.env.MONGO_URI en vez de la liga
+    */
+    urlBD = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlBD;
